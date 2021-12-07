38f540ff3363e2b6da931c0d55e4a1ec1f7d6e15
Siddhant Attavar <51072192+SiddhantAttavar@users.noreply.github.com>
2021-12-07 15:23:32 +0530

    Add Medium Bot

--- Bot.py
+++ Bot.py
@@ -35,3 +35,59 @@ def easyBot(game):
 
 	# Select a random cell from game.remainingCells
 	return choice(list(game.remainingCells))
+
+def mediumBot(game):
+	'''Returns the move for the medium mode bot
+
+	This bot selects any immediate possibility of a win.
+	If this is not possible, it tries to block any immediate possibility of a loss.
+	Otherwise, it selects a random cell
+
+	Parameters
+	----------
+	game : Game
+		The current game board
+	
+	Returns
+	-------
+	Tuple[int, int]
+		The next move of the medium bot
+	'''
+
+	# Check if any cell will provide an immediate win
+	emptyCells = list(game.remainingCells)
+	for x, y in emptyCells:
+		# Try selecting the cell
+		game.makeMove((x, y))
+		
+		# Revert to previous turn
+		game.board[y][x] = None
+		game.remainingCells.add((x, y))
+		game.turn = not game.turn
+
+		# Check if this cell is an immediate win
+		if game.gameOver:
+			game.gameOver = False
+			return (x, y)
+
+	# Check if any cell will block an immediate opponent win
+	if len(emptyCells) > 1:
+		game.turn = not game.turn
+		for x, y in emptyCells:
+			# Try selecting the cell
+			game.makeMove((x, y))
+			
+			# Revert to previous turn
+			game.board[y][x] = None
+			game.remainingCells.add((x, y))
+			game.turn = not game.turn
+
+			# Check if this cell blocks anything
+			if game.gameOver:
+				game.gameOver = False
+				game.turn = not game.turn
+				return (x, y)
+		game.turn = not game.turn
+	
+	# Select a random cell from game.remainingCells
+	return choice(emptyCells)
